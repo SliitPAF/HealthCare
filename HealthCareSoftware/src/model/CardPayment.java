dd7c7bdafec73c9e94f1e3a48caae0f3591adc86
Lakshan Wijewardana <48085984+shanwij@users.noreply.github.com>
2020-10-02 23:32:17 +0530

    Update CardPayment.java

--- HealthCareSoftware/src/model/CardPayment.java
+++ HealthCareSoftware/src/model/CardPayment.java
@@ -12,34 +12,43 @@ public class CardPayment {
 	public CardPayment(){
 		
 	}
-	
+	//get card no
 	public String getCreditCardNumber() {
 		return creditCardNumber;
 	}
+	//set card no
 	public void setCreditCardNumber(String creditCardNumber) {
 		this.creditCardNumber = creditCardNumber;
 	}
+	//get amount
 	public float getAmount() {
 		return amount;
 	}
+	//set amount
 	public void setAmount(float amount) {
 		this.amount = amount;
 	}
+	//get cvc
 	public int getCvc() {
 		return cvc;
 	}
+	//set cvc
 	public void setCvc(int cvc) {
 		this.cvc = cvc;
 	}
+	//get card holder fname
 	public String getCardHolderFName() {
 		return cardHolderFName;
 	}
+	//set card holder fname
 	public void setCardHolderFName(String cardHolderFName) {
 		this.cardHolderFName = cardHolderFName;
 	}
+	//get card holder lname
 	public String getCardHolderLName() {
 		return cardHolderLName;
 	}
+	//set card holder lname
 	public void setCardHolderLName(String cardHolderLName) {
 		this.cardHolderLName = cardHolderLName;
 	}
